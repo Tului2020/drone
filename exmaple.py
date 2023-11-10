29e533f8350baa9117b80e65bb8a9c79e71d4c5f
tului-linux <tului_2020@yahoo.com>
2023-11-10 15:39:28 -0800

    Test

---
+++ exmaple.py
@@ -0,0 +1,15 @@
+import sys
+from msp.multiwii import MultiWii
+
+if __name__ == "__main__":
+    try:
+        print_debug = sys.argv[1].lower() == 'true'
+        fc = MultiWii("/dev/ttyS0", print_debug)
+        fc.start()
+        while True:
+            print(fc.get_attitude())
+
+    except Exception as error:
+        import traceback
+        print("Error on Main: " + str(error))
+        traceback.print_exc()
