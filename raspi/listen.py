bfc85c593eb746ed01da98611ac3eb82653de9a8
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-11 11:18:48 -0800

    Test

--- raspi/listen.py
+++ raspi/listen.py
@@ -1,4 +1,5 @@
 import connection
+import time
 
 ser = connection.ser
 
@@ -7,6 +8,8 @@ try:
         if ser.in_waiting > 0:
             received_data = ser.read(4).decode('utf-8')  # Read and decode data
             print("Received:", received_data)
+            time.sleep(0.1)
+
 
 except KeyboardInterrupt:
     pass
