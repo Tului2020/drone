f3c05c3d9992813b3a0392acc4db775d92699ffb
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-11 11:13:42 -0800

    Test

--- raspi/send.py
+++ raspi/send.py
@@ -1,7 +1,7 @@
-# from .connection import ser
+from .connection import ser
 
 message_count = 0
 while True:
     message = input(f'{message_count}. message to be sent: ')
-    # ser.write
+    ser.write(str.encode(message))
     message_count += 1
