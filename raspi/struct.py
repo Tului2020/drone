7e4e138d8ade1b5e8f88789e1b1cdd56fb66bf68
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-12 15:28:46 -0800

    Test

--- raspi/struct.py
+++ raspi/struct.py
@@ -3,6 +3,6 @@ import connection
 
 ser = connection.ser
 
-string = struct.pack('cccBBB', '$', 'M', '<', 0, 100, 100)  # simple MSP_IDENT
+string = struct.pack('cccBBB', b'$', b'M', b'<', 0, 100, 100)  # simple MSP_IDENT
 print(ser.write(string))
 
