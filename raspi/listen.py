5ec078d558c4ddbacd7971a74ddd92e3706190fc
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-12 10:25:01 -0800

    Test

--- raspi/listen.py
+++ raspi/listen.py
@@ -6,7 +6,7 @@ ser = connection.ser
 try:
     while True:
         if ser.in_waiting > 0:
-            received_data = ser.read(4).decode('utf-8')  # Read and decode data
+            received_data = ser.read(4)  # Read and decode data
             print("Received:", received_data)
             time.sleep(0.001)
 
