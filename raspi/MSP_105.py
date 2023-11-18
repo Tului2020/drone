2f624d7af24ef67b9d96443123609f4825af65a2
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-18 13:39:49 -0800

    test

--- raspi/MSP_105.py
+++ raspi/MSP_105.py
@@ -3,51 +3,45 @@ import time
 
 ser = connection.ser
 
-MSP_RC = 105  # 105
-MSP_SET_RAW_RC = 200  # 200
-message_id = MSP_RC
-payload = [0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07,
-           0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0, 0x07, 0xD0]
-size = len(payload)
+MSP_RC = 105
+header = [36, 77, 60]
+size_message_id = [16, MSP_RC]
 
-byte_header = b'$M>'
-byte_size = bytes([size])
-byte_message_id = bytes([message_id])
-byte_payload = bytes(payload)
+throttle = 1500
+roll = 1500
+pitch = 1500
+yaw = 1500
+aux1 = 1500
+aux2 = 1500
+aux3 = 0
+aux4 = 0
 
+payload_bytes = [
+    throttle.to_bytes(2, 'little'),
+    roll.to_bytes(2, 'little'),
+    pitch.to_bytes(2, 'little'),
+    yaw.to_bytes(2, 'little'),
+    aux1.to_bytes(2, 'little'),
+    aux2.to_bytes(2, 'little'),
+    aux3.to_bytes(2, 'little'),
+    aux4.to_bytes(2, 'little'),
+]
+payload = []
 checksum = 0
-checksum ^= size
-checksum ^= message_id
-for payload_byte in byte_payload:
-    checksum ^= payload_byte
-byte_checksum = bytes([checksum])
+for i in [*size_message_id, *payload]:
+    if (type(i) == int):
+        checksum ^= i
+    else:
+        payload.append(i[0])
+        checksum ^= i[0]
 
-print('<--------------------------------------------------->')
-print('size             ', size)
-print('payload          ', payload)
-print('message_id       ', message_id)
-print('checksum         ', checksum)
-print()
-print('byte_header      ', byte_header)
-print('byte_size        ', byte_size)
-print('byte_message_id  ', byte_message_id)
-print('byte_payload     ', byte_payload)
-print('byte_checksum    ', byte_checksum)
-print('<--------------------------------------------------->')
+        payload.append(i[1])
+        checksum ^= i[1]
+
+message = [*header, *size_message_id, *payload, checksum]
 
 while True:
     print("sending data...")
-    # ser.write(bytes([36, 77, 60, 16, 200, 220, 5, 120, 5, 176, 4, 0, 0, 0, 0, 0, 0, 0, 0, 17]))
-    ser.write(bytes([
-        36, 77, 60, 16, MSP_SET_RAW_RC,
-        220, 5,     # Throttle
-        120, 5,     # Roll
-        220, 5,     # Pitch
-        176, 4,     # Yaw
-        0, 0,     # AUX1
-        0, 0,     # AUX2
-        0, 0,       # AUX3
-        0, 0,       # AUX4
-        17,
-    ]))
+    # ser.write(bytes([16, 200, 220, 5, 120, 5, 220, 5, 176, 4, 0, 0, 0, 0, 0, 0, 0, 0]))
+    ser.write(bytes(message))
     time.sleep(0.5)
---
+++ raspi/test.py
@@ -0,0 +1,4 @@
+throttle = 1500
+
+
+print(throttle.to_bytes(2, 'little'))
