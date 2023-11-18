7994411848f19733357753755106c4c21649e5bf
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-18 11:02:18 -0800

    test

--- raspi/MSP_108.py
+++ raspi/MSP_108.py
@@ -5,31 +5,17 @@ ser = connection.ser
 
 MSP_ATTITUDE = 0x6C # 108
 message_id = MSP_ATTITUDE
-payload = [0]
-size = len(payload)
 
 byte_header = b'$M<'
 byte_size = bytes([0])
 byte_message_id = bytes([message_id])
-byte_payload = bytes(payload)
-
-checksum = 0
-checksum ^= 0x20
-checksum ^= message_id
-for payload_byte in byte_payload:
-    checksum ^= payload_byte
-byte_checksum = bytes([108])
+byte_checksum = bytes([message_id])
 
 print('<--------------------------------------------------->')
-print('size             ', size)
-print('payload          ', payload)
 print('message_id       ', message_id)
-print('checksum         ', checksum)
-print()
 print('byte_header      ', byte_header)
 print('byte_size        ', byte_size)
 print('byte_message_id  ', byte_message_id)
-print('byte_payload     ', byte_payload)
 print('byte_checksum    ', byte_checksum)
 print('<--------------------------------------------------->')
 
