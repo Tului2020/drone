6748d039763cc51a63b33498e6056e7b9d628b39
Tului Gantulga <tgantulga@ucdavis.edu>
2023-11-18 11:50:16 -0800

    test

--- raspi/listen.py
+++ raspi/listen.py
@@ -8,19 +8,21 @@ message_parse_idx = 0
 message_array = []
 message_size = 0
 
-def reset_buffer():
-    message_parse_idx = 0
-    message_array = []
-    message_size = 0
-
 def parse_message(raw_message):
+    global message_parse_idx
+    global message_size
+    global message_id
+    global message_array
+
     for _byte in raw_message:
         if (message_parse_idx < 3):
             if (chr(_byte) == message_header[message_parse_idx]):
                 message_array.append(chr(_byte))
                 message_parse_idx += 1
             else:
-                reset_buffer()
+                message_parse_idx = 0
+                message_array = []
+                message_size = 0
         if (message_parse_idx == 3):
             message_parse_idx += 1
             message_size = int.from_bytes(_byte)
