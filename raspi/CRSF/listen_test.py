b8401660575a39d163728709ecf7f048b0cec1ee
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-07 15:17:13 -0800

    test

--- raspi/CRSF/listen_test.py
+++ raspi/CRSF/listen_test.py
@@ -5,10 +5,11 @@ iter = 0
 try:
     while True:
         if ser.in_waiting > 0:
-            print(f'iteration: {iter}')
             received_data = ser.read(1)
+            print(f'iteration: {iter}')
             print('received_data', received_data)
             print('received_data', int.from_bytes(received_data))
+            iter += 1
 
 
 except KeyboardInterrupt:
