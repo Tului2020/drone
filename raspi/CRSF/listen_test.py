a7cf63a34cd671ee66ff86b2785ece26216e1a4e
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-07 15:39:29 -0800

    test

--- raspi/CRSF/listen_test.py
+++ raspi/CRSF/listen_test.py
@@ -6,9 +6,13 @@ try:
     while True:
         if ser.in_waiting > 0:
             received_data = ser.read(1)
+            print()
             print(f'iteration: {iter}')
             print('received_data', received_data)
             print('received_data', int.from_bytes(received_data))
+            if (received_data == 0xC8 or received_data == 0xEE):
+                print('FOUND')
+
             iter += 1
 
 
