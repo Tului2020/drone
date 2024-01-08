a1d4cd48e2638b0dc9bb713463543b33ad6245c1
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-07 17:11:37 -0800

    test

--- raspi/CRSF/example.py
+++ raspi/CRSF/example.py
@@ -105,6 +105,7 @@ with serial.Serial(args.port, args.baud, timeout=2) as ser:
             input.extend(ser.read(ser.in_waiting))
         else:
             time.sleep(0.5)
+        print('\n\n')
         print('input', len(input))
         if len(input) > 2:
             # This simple parser works with malformed CRSF streams
