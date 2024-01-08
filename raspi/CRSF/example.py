72a1ab5b216b833ea4c1a9d5c74ca85de74ea5bf
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-07 17:14:13 -0800

    test

--- raspi/CRSF/example.py
+++ raspi/CRSF/example.py
@@ -103,11 +103,11 @@ with serial.Serial(args.port, args.baud, timeout=2) as ser:
     while True:
         if ser.in_waiting > 0:
             input.extend(ser.read(ser.in_waiting))
-        else:
-            time.sleep(0.020)
-        print('\n\n')
-        print('input', len(input))
+        # else:
+        #     time.sleep(0.020)
         if len(input) > 2:
+            print('\n')
+            print('input', len(input))
             # This simple parser works with malformed CRSF streams
             # it does not check the first byte for SYNC_BYTE, but
             # instead just looks for anything where the packet length
