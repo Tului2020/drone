81631aa1b1067a597ec3c136cef6484a036a6cad
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-09 10:41:48 -0800

    test

--- raspi/MAVlink/test.py
+++ raspi/MAVlink/test.py
@@ -1,4 +1,5 @@
 from pymavlink import mavutil
+from time import sleep
 
 # Set the UART port and baud rate
 uart_port = '/dev/ttyS0'  # Example for Linux
@@ -6,19 +7,29 @@ uart_port = '/dev/ttyS0'  # Example for Linux
 baud_rate = 57600
 
 def main():
-    # Open the serial port
+    # # Open the serial port
     mav_connection = mavutil.mavlink_connection(uart_port, baud=baud_rate, dialect="standard", mavversion="2.0")
 
-    print("Waiting for MAVLink messages...")
+    # print("Waiting for MAVLink messages...")
+    # while True:
+    #     try:
+    #         # Wait for a valid MAVLink message
+    #         msg = mav_connection.recv_match(blocking=True)
+    #         if msg:
+    #             print(f"Received message: {msg.to_dict()}")
+    #     except Exception as e:
+    #         print(f"Error: {e}")
+    #         break
+
+    mav_connection.wait_heartbeat()
+
+    # Get some information !
     while True:
         try:
-            # Wait for a valid MAVLink message
-            msg = mav_connection.recv_match(blocking=True)
-            if msg:
-                print(f"Received message: {msg.to_dict()}")
-        except Exception as e:
-            print(f"Error: {e}")
-            break
+            print(mav_connection.recv_match().to_dict())
+        except:
+            pass
+        sleep(0.1)
 
 if __name__ == "__main__":
     main()
