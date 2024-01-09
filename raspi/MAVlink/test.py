7627e1f2362fabdd88a2c9738beb55caf0adc5b9
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-09 10:26:42 -0800

    test

--- raspi/MAVlink/test.py
+++ raspi/MAVlink/test.py
@@ -11,14 +11,18 @@ def main():
     ser = serial.Serial(uart_port, baud_rate, timeout=1)
 
     # Create a mavlink connection, specifying the serial port
-    print(ser.port)
-    mav_connection = mavutil.mavlink_connection(ser.port)
+    # mav_connection = mavutil.mavlink_connection(ser.port)
+    # # For MAVLink 2
+    # mavlink2_connection = mavutil.mavlink_connection(uart_port, baud=57600, dialect="standard", mavversion="2.0")
+
+    # For MAVLink 1
+    mavlink1_connection = mavutil.mavlink_connection(uart_port, baud=57600, dialect="standard", mavversion="1.0")
 
     print("Waiting for MAVLink messages...")
     # while True:
     try:
         # Wait for a valid MAVLink message
-        msg = mav_connection.recv_match(blocking=True)
+        msg = mavlink1_connection.recv_match(blocking=True)
         if msg:
             print(f"Received message: {msg.to_dict()}")
     except Exception as e:
