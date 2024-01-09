8aed6bbd955af4636db8e54b1fc5e8ffd829f68f
Tului Gantulga <tgantulga@ucdavis.edu>
2024-01-09 10:51:28 -0800

    test

--- raspi/MAVlink/test.py
+++ raspi/MAVlink/test.py
@@ -10,18 +10,16 @@ def main():
     # # Open the serial port
     mav_connection = mavutil.mavlink_connection(uart_port, baud=baud_rate, dialect="standard", mavversion="2.0")
 
-    # print("Waiting for MAVLink messages...")
-    # while True:
-    #     try:
-    #         # Wait for a valid MAVLink message
-    #         msg = mav_connection.recv_match(blocking=True)
-    #         if msg:
-    #             print(f"Received message: {msg.to_dict()}")
-    #     except Exception as e:
-    #         print(f"Error: {e}")
-    #         break
-
-    mav_connection.wait_heartbeat()
+    print("Waiting for MAVLink messages...")
+    while True:
+        try:
+            # Wait for a valid MAVLink message
+            msg = mav_connection.recv_match(blocking=True)
+            if msg:
+                print(f"Received message: {msg.to_dict()}")
+        except Exception as e:
+            print(f"Error: {e}")
+            break
 
     # Get some information !
     while True:
