75dee31fff9882414a3a5ec3ef0e73160f8803a3
Tului Gantulga <tgantulga@ucdavis.edu>
2024-02-24 02:31:13 -0800

    test

--- raspi/CRSF/DEVICE_INFO_listen.py
+++ raspi/CRSF/DEVICE_INFO_listen.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env python3
 import serial
+import time
 import argparse
 
 from connection import PacketsTypes
@@ -42,20 +43,22 @@ with serial.Serial(args.port, args.baud, timeout=2) as ser:
     while True:
         if ser.in_waiting > 0:
             input.extend(ser.read(ser.in_waiting))
-        # else:
-        #     time.sleep(0.020)
+        else:
+            time.sleep(0.020)
         if len(input) > 2:
             # print('\n')
-            print('input', len(input))
+            # print('input', len(input))
             # This simple parser works with malformed CRSF streams
             # it does not check the first byte for SYNC_BYTE, but
             # instead just looks for anything where the packet length
             # is 4-64 bytes, and the CRC validates
             expected_len = input[1] + 2
+            print(f"expected_len {expected_len}")
             # print('expected_len', expected_len)
             if expected_len > 64 or expected_len < 4:
                 input = []
             elif len(input) >= expected_len:
+                print(f"input {input}")
                 single = input[:expected_len] # copy out this whole packet
                 input = input[expected_len:] # and remove it from the buffer
 
