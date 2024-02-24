69387c99ebab14f8de6b74e11d2bbbbae7752295
Tului Gantulga <tgantulga@ucdavis.edu>
2024-02-24 02:12:52 -0800

    test

--- raspi/CRSF/DEVICE_INFO_listen.py
+++ raspi/CRSF/DEVICE_INFO_listen.py
@@ -27,56 +27,10 @@ def signed_byte(b):
     return b - 256 if b >= 128 else b
 
 def handleCrsfPacket(ptype, data):
-    if ptype == PacketsTypes.RADIO_ID and data[5] == 0x10:
-        print(f"OTX sync")
-        pass
-    elif ptype == PacketsTypes.LINK_STATISTICS:
-        rssi1 = signed_byte(data[3])
-        rssi2 = signed_byte(data[4])
-        lq = data[5]
-        snr = signed_byte(data[6])
-        antenna = data[7]
-        mode = data[8]
-        power = data[9]
-        # telemetry strength
-        downlink_rssi = signed_byte(data[10])
-        downlink_lq = data[11]
-        downlink_snr = signed_byte(data[12])
-        print(f"RSSI={rssi1}/{rssi2}dBm LQ={lq:03} mode={mode} ant={antenna} snr={snr} power={power} drssi={downlink_rssi} dlq={downlink_lq} dsnr={downlink_snr}")
-    elif ptype == PacketsTypes.ATTITUDE:
-        pitch = int.from_bytes(data[3:5], byteorder='big', signed=True) / 10000.0
-        roll = int.from_bytes(data[5:7], byteorder='big', signed=True) / 10000.0
-        yaw = int.from_bytes(data[7:9], byteorder='big', signed=True) / 10000.0
-        print(f"Attitude: Pitch={pitch:0.2f} Roll={roll:0.2f} Yaw={yaw:0.2f} (rad)")
-    elif ptype == PacketsTypes.FLIGHT_MODE:
-        packet = ''.join(map(chr, data[3:-2]))
-        print(f"Flight Mode: {packet}")
-    elif ptype == PacketsTypes.BATTERY_SENSOR:
-        vbat = int.from_bytes(data[3:5], byteorder='big', signed=True) / 10.0
-        curr = int.from_bytes(data[5:7], byteorder='big', signed=True) / 10.0
-        mah = data[7] << 16 | data[8] << 7 | data[9]
-        pct = data[10]
-        print(f"Battery: {vbat:0.2f}V {curr:0.1f}A {mah}mAh {pct}%")
-    elif ptype == PacketsTypes.DEVICE_INFO:
+    if ptype == PacketsTypes.DEVICE_INFO:
         packet = ' '.join(map(hex, data))
         print(f"Device Info: {packet}")
-    elif data[2] == PacketsTypes.GPS:
-        lat = int.from_bytes(data[3:7], byteorder='big', signed=True) / 1e7
-        lon = int.from_bytes(data[7:11], byteorder='big', signed=True) / 1e7
-        gspd = int.from_bytes(data[11:13], byteorder='big', signed=True) / 36.0
-        hdg =  int.from_bytes(data[13:15], byteorder='big', signed=True) / 100.0
-        alt = int.from_bytes(data[15:17], byteorder='big', signed=True) - 1000
-        sats = data[17]
-        print(f"GPS: Pos={lat} {lon} GSpd={gspd:0.1f}m/s Hdg={hdg:0.1f} Alt={alt}m Sats={sats}")
-    elif ptype == PacketsTypes.VARIO:
-        vspd = int.from_bytes(data[3:5], byteorder='big', signed=True) / 10.0
-        print(f"VSpd: {vspd:0.1f}m/s")
-    elif ptype == PacketsTypes.RC_CHANNELS_PACKED:
-        print(f"Channels: (data)")
-        pass
-    else:
-        packet = ' '.join(map(hex, data))
-        # print(f"Unknown 0x{ptype:02x}: {packet}")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-P', '--port', default='/dev/ttyS0', required=False)
