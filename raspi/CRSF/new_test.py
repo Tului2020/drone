16f90f3528f0cda2d05c0ae666d11a658bc87a79
Tului Gantulga <tgantulga@ucdavis.edu>
2024-02-24 01:15:10 -0800

    test

---
+++ raspi/CRSF/new_test.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+# Initialize serial connection
+ser = serial.Serial('/dev/serial0', 57600, timeout=1)  # Adjust serial port and baud rate as necessary
+
+while True:
+    # Example: Sending a command to the FC
+    # ser.write(b'YourCommandHere\n')
+    
+    # Example: Reading data from the FC
+    line = ser.readline().decode('utf-8').rstrip()
+    if line:
+        print("Received:", line)
+    
+    time.sleep(1)
