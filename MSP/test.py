980ac5fb7a369792757d6559f4b3904ddef4f715
tului-linux <tului_2020@yahoo.com>
2024-03-22 14:23:31 -0700

    test

--- MSP/test.py
+++ MSP/test.py
@@ -1,9 +1,9 @@
-from shell import board
+# from shell import board
 from time import sleep, time
 
 total_time = 3
-start_speed = 1000
-end_speed = 1400
+start_speed = 10
+end_speed = 14
 
 half_time = total_time / 2
 delta_speed =  end_speed - start_speed
@@ -13,18 +13,16 @@ start_timestamp = time()
 for i in range(start_speed, end_speed):
     time_passed = time() - start_timestamp
     speed = i
-    board.set_motor_individual(speed, speed, speed, speed)
-    if (i % 10) == 0:
+    # board.set_motor_individual(speed, speed, speed, speed)
+    if (i % 1) == 0:
         print(round(time_passed), delta_time, speed)
-        
-    print(delta_time, i)
     sleep(delta_time)
 
-for i in range(start_speed, end_speed):
+for s in range(start_speed, end_speed + 1):
     time_passed = time() - start_timestamp
-    speed = end_speed - i
-    board.set_motor_individual(speed, speed, speed, speed)
-    if (i % 100) == 0:
+    speed = end_speed - s + start_speed
+    # board.set_motor_individual(speed, speed, speed, speed)
+    if (i % 1) == 0:
         print(round(time_passed), delta_time, speed)
     sleep(delta_time)
 
