e51923b69b36b126cc93b79ee73efd1569bce0a9
tului-linux <tului_2020@yahoo.com>
2024-03-24 12:23:34 -0700

    test

--- MSP/test.py
+++ MSP/test.py
@@ -2,20 +2,20 @@ from shell import board
 
 board.feature_jump()
 
-while True:
-    try:
-        data = board.get_attitude()
-        pitch = data['angy']
-        roll = data['angx']
-        heading = data['heading']
+# while True:
+#     try:
+#         data = board.get_attitude()
+#         pitch = data['angy']
+#         roll = data['angx']
+#         heading = data['heading']
 
-        speed_motor_1 = pitch * 10
-        speed_motor_2 = pitch * 10
-        speed_motor_3 = pitch * 10
-        speed_motor_4 = pitch * 10
-        print(speed_motor_1)
-        board.set_motor_individual(speed_motor_1, speed_motor_2, speed_motor_3, speed_motor_4)
+#         speed_motor_1 = pitch * 10
+#         speed_motor_2 = pitch * 10
+#         speed_motor_3 = pitch * 10
+#         speed_motor_4 = pitch * 10
+#         print(speed_motor_1)
+#         board.set_motor_individual(speed_motor_1, speed_motor_2, speed_motor_3, speed_motor_4)
 
-    except:
-        pass
+#     except:
+#         pass
 
