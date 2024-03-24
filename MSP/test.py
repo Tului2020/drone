0fe9a51f88e0cb83c431447a0df9332938dedd30
tului-linux <tului_2020@yahoo.com>
2024-03-24 12:20:46 -0700

    test

--- MSP/test.py
+++ MSP/test.py
@@ -3,7 +3,17 @@ from shell import board
 while True:
     data = board.get_attitude()
     try:
-        print(data)
+        pitch = data['angy']
+        roll = data['angx']
+        heading = data['heading']
+
+        speed_motor_1 = pitch * 10
+        speed_motor_2 = pitch * 10
+        speed_motor_3 = pitch * 10
+        speed_motor_4 = pitch * 10
+
+        board.set_motor_individual(speed_motor_1, speed_motor_2, speed_motor_3, speed_motor_4)
+
     except:
         pass
 
