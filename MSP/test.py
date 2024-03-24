d15a4fb9c5414315c266ed2267ebd7edba45f436
tului-linux <tului_2020@yahoo.com>
2024-03-24 12:16:20 -0700

    test

--- MSP/test.py
+++ MSP/test.py
@@ -1,33 +1,9 @@
 from shell import board
-from pprint import pprint
-
-# board.feature_jump()
-
-def format_data(data):
-    format_str = "{:0}: {:+5.2f}; "
-    return_string = ""
-
-    for key in ['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'mx', 'my', 'mz']:
-        try:
-            return_string += format_str.format(key, data[key])
-        except:
-            pass
-    
-    return return_string
 
 while True:
-    data = board.get_imu()
+    data = board.get_attitude()
     try:
-        print(data['ax'], data['gx'], data['mx'])
+        print(data)
     except:
         pass
-    # pprint(x['ax'])
-    # # print(board.get_attitude())
-
-    # # print(board.get_imu())
-    # format_str = "{:2}: {:+7.2f}"
-
-    # # Printing each item with consistent formatting
-    # for key, value in data.items():
-    #     print(format_str.format(key.upper(), value))
 
