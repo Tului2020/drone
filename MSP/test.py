979a7c289a7d35aaae28967ab6777c4cecfd49d0
tului-linux <tului_2020@yahoo.com>
2024-03-24 12:21:31 -0700

    test

--- MSP/test.py
+++ MSP/test.py
@@ -11,7 +11,7 @@ while True:
         speed_motor_2 = pitch * 10
         speed_motor_3 = pitch * 10
         speed_motor_4 = pitch * 10
-
+        print(speed_motor_1)
         board.set_motor_individual(speed_motor_1, speed_motor_2, speed_motor_3, speed_motor_4)
 
     except:
