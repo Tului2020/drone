fb1ad8a64ef4fbd4779558f4f31c0402812165bd
Tului Gantulga <tului_2020@yahoo.com>
2024-04-04 08:57:15 -0700

    test

---
+++ MSP/stabilze.py
@@ -0,0 +1,25 @@
+# Stablize the drone using the MSP library
+from shell import board
+from time import sleep
+
+MAX_SPEED = 1200
+MIN_SPEED = 1000
+
+try:
+    while True:
+        attitude = board.getData(board.ATTITUDE)
+        angx = attitude['angx']
+        angy = attitude['angy']
+        heading = attitude['heading']
+
+        # Stablize the drone
+        # TEST
+
+
+        sleep(0.02)
+
+except KeyboardInterrupt:
+    print("Program stopped by user")
+    board.ser.close()
+    print("Serial port closed")
+    exit()
