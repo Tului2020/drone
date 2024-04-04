863ae07e70fa2a745ba2609975f1da3ef4e7fb5d
Tului Gantulga <tului_2020@yahoo.com>
2024-04-04 08:53:08 -0700

    added PID controller base

--- MSP/util.py
+++ MSP/util.py
@@ -8,3 +8,19 @@ def push16(buf, val):
 
     # high byte
     push8(buf, val >> 8)
+
+class PIDController:
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.integral = 0
+        self.previous_error = 0
+
+    def update(self, setpoint, measured_value, time_delta):
+        error = setpoint - measured_value
+        self.integral += error * time_delta
+        derivative = (error - self.previous_error) / time_delta
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+        self.previous_error = error
+        return output
