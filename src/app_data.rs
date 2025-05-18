35004a4996fd04a2b4e6c6d5d370f9af592e18ab
Tului Gantulga <tului_2020@yahoo.com>
2025-05-17 23:17:36 -0700

    updated app_data

--- src/app_data.rs
+++ src/app_data.rs
@@ -1,4 +1,22 @@
 //! App Data needed for the drone application
 
-/// Donr App Data
-pub struct DroneAppData {}
+use serde::{Deserialize, Serialize};
+
+use crate::logger::LogLevel;
+
+/// Drone App Data
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DroneAppData {
+    /// App log level
+    log_level: LogLevel,
+
+    /// Port name for serial connection to the Flight Controller (FC)
+    ///
+    /// Default "/dev/ttyS0"
+    fc_port_name: String,
+
+    /// FC connection baud rate
+    ///
+    /// Default 420_000 baud rate
+    fc_baud_rate: u32,
+}
