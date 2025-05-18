ce3bc16444b0d16f36eeef1fac726a09761a7ed8
Tului Gantulga <tului_2020@yahoo.com>
2025-05-17 23:16:13 -0700

    added log levels

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,6 @@
 //! Contains the logger for the cloud-comms application.
 use crate::DroneResult;
+use serde::{Deserialize, Serialize};
 use tracing_subscriber::FmtSubscriber;
 
 /// Initializes the logger for the Conductor application.
@@ -10,4 +11,43 @@ pub fn init_logger() -> DroneResult<()> {
     // Set the subscriber as the global default
     tracing::subscriber::set_global_default(subscriber)?;
     Ok(())
+}
+
+/// Application log level
+#[derive(Debug, Serialize, Deserialize)]
+pub enum LogLevel {
+    /// Trace level
+    TRACE,
+    /// Debug level
+    DEBUG,
+    /// Info level
+    INFO,
+    /// Warn level
+    WARN,
+    /// Error level
+    ERROR,
+}
+
+impl From<LogLevel> for tracing::Level {
+    fn from(level: LogLevel) -> Self {
+        match level {
+            LogLevel::TRACE => Self::TRACE,
+            LogLevel::DEBUG => Self::DEBUG,
+            LogLevel::INFO => Self::INFO,
+            LogLevel::WARN => Self::WARN,
+            LogLevel::ERROR => Self::ERROR,
+        }
+    }
+}
+
+impl From<tracing::Level> for LogLevel {
+    fn from(level: tracing::Level) -> Self {
+        match level {
+            tracing::Level::TRACE => Self::TRACE,
+            tracing::Level::DEBUG => Self::DEBUG,
+            tracing::Level::INFO => Self::INFO,
+            tracing::Level::WARN => Self::WARN,
+            tracing::Level::ERROR => Self::ERROR,
+        }
+    }
 }
