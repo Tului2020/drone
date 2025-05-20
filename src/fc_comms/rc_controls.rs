86cf0cd39d2e324831329dbefc32aa43076fff6d
Tului Gantulga <tului_2020@yahoo.com>
2025-05-19 23:16:34 -0700

    added docs

--- src/fc_comms/rc_controls.rs
+++ src/fc_comms/rc_controls.rs
@@ -1,19 +1,29 @@
 //! RcControls struct
 use std::fmt::Display;
 
+/// Struct to hold the RC controls values
 pub struct RcControls {
+    /// Roll channel value
     pub roll: u16,
+    /// Pitch channel value
     pub pitch: u16,
+    /// Yaw channel value
     pub yaw: u16,
+    /// Throttle channel value
     pub thr: u16,
+    /// Auxiliary channel 1 value
     pub aux1: u16,
+    /// Auxiliary channel 2 value
     pub aux2: u16,
+    /// Auxiliary channel 3 value
     pub aux3: u16,
+    /// Auxiliary channel 4 value
     pub aux4: u16,
 }
 
 impl RcControls {
     #[cfg(feature = "real")]
+    /// Converts the RcControls struct to a byte array for real drone communication
     pub fn chans_us(&self) -> [u16; 16] {
         [
             self.roll, self.pitch, self.thr, self.yaw, self.aux1, self.aux2, self.aux3, self.aux4,
