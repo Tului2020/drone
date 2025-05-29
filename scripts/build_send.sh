8ac1c5659da27ac0890e42be6d01100fdf95cc79
Tului Gantulga <tului_2020@yahoo.com>
2025-05-28 20:56:34 -0700

    updated build_send

--- scripts/build_send.sh
+++ scripts/build_send.sh
@@ -1,1 +1,9 @@
-cross build --target=aarch64-unknown-linux-gnu --release --no-default-features --features raspi --features heartbeat && sudo scp ./target/aarch64-unknown-linux-gnu/release/drone drone@drone.local:/home/drone
+# brew install --cask docker         # Desktop edition on macOS
+# rustup component add rust-src      # cross needs it
+# cargo install cross --git https://github.com/cross-rs/cross --locked
+
+# build for pi os 32-bit
+cross build --target armv7-unknown-linux-gnueabihf --release --no-default-features --features raspi --features heartbeat && scp ./target/armv7-unknown-linux-gnueabihf/release/drone drone@drone.local:/home/drone
+
+# build for pi os 64-bit
+# cross build --target=aarch64-unknown-linux-gnu --release --no-default-features --features raspi --features heartbeat && sudo scp ./target/aarch64-unknown-linux-gnu/release/drone
