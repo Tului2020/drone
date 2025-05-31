7ce500638baaa36d61789f3102a2c9449641dbe3
Tului Gantulga <tului_2020@yahoo.com>
2025-05-30 21:29:46 -0700

    cleaned code

--- src/bin/control_server.rs
+++ src/bin/control_server.rs
@@ -1,4 +1,3 @@
-#[cfg(feature = "control_server")]
 use std::{
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -7,50 +6,43 @@ use std::{
     time::Duration,
 };
 
-#[cfg(feature = "control_server")]
-use drone::control_server::ControlServer;
-use drone::DroneResult;
-#[cfg(feature = "control_server")]
+use drone::{control_server::ControlServer, DroneResult};
 use tokio::time::sleep;
-#[cfg(feature = "control_server")]
 use tracing::{debug, error, info};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> DroneResult {
-    #[cfg(feature = "control_server")]
-    {
-        let running = Arc::new(AtomicBool::new(true));
-        let running_clone = running.clone();
+    let running = Arc::new(AtomicBool::new(true));
+    let running_clone = running.clone();
 
-        let control_server = ControlServer::new("./config.json")?;
+    let control_server = ControlServer::new("./config.json")?;
 
-        ctrlc::set_handler(move || {
-            debug!("Ctrl+C detected!");
-            running_clone.store(false, Ordering::SeqCst);
-        })
-        .expect("Error setting Ctrl-C handler");
+    ctrlc::set_handler(move || {
+        debug!("Ctrl+C detected!");
+        running_clone.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
 
-        let ctrlc_task = tokio::spawn(async move {
-            while running.load(Ordering::SeqCst) {
-                sleep(Duration::from_secs(1)).await;
-            }
-        });
+    let ctrlc_task = tokio::spawn(async move {
+        while running.load(Ordering::SeqCst) {
+            sleep(Duration::from_secs(1)).await;
+        }
+    });
 
-        let control_server_task = tokio::spawn(async move {
-            if let Err(e) = control_server.start().await {
-                error!("Error starting control server: {e}");
-            }
-        });
+    let control_server_task = tokio::spawn(async move {
+        if let Err(e) = control_server.start().await {
+            error!("Error starting control server: {e}");
+        }
+    });
 
-        tokio::select! {
-            _ = ctrlc_task => {
-                info!("Ctrl-C handler task completed.");
-            }
-            _ = control_server_task => {
-                info!("Control server task completed.");
-            }
-        };
-    }
+    tokio::select! {
+        _ = ctrlc_task => {
+            info!("Ctrl-C handler task completed.");
+        }
+        _ = control_server_task => {
+            info!("Control server task completed.");
+        }
+    };
 
     Ok(())
 }
