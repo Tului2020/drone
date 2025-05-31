75ce442d83e4d1c00d21fbbe2c7e023d79d2cdbc
Tului Gantulga <tului_2020@yahoo.com>
2025-05-31 15:24:33 -0700

    removed heartbeat feature from build_send.sh

--- scripts/build_send.sh
+++ scripts/build_send.sh
@@ -3,7 +3,7 @@
 # cargo install cross --git https://github.com/cross-rs/cross --locked
 
 # build for pi os 32-bit
-cross build --target armv7-unknown-linux-gnueabihf --release --no-default-features --features raspi --features heartbeat && scp ./target/armv7-unknown-linux-gnueabihf/release/drone drone@drone.local:/home/drone
+cross build --target armv7-unknown-linux-gnueabihf --release --no-default-features --features raspi && scp ./target/armv7-unknown-linux-gnueabihf/release/drone drone@drone.local:/home/drone
 
 # build for pi os 64-bit
-# cross build --target=aarch64-unknown-linux-gnu --release --no-default-features --features raspi --features heartbeat && sudo scp ./target/aarch64-unknown-linux-gnu/release/drone
+# cross build --target=aarch64-unknown-linux-gnu --release --no-default-features --features raspi && sudo scp ./target/aarch64-unknown-linux-gnu/release/drone
